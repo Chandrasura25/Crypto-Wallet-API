1d536ca4df49e7a81f627064bfa330c88ac9f9cc
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-02-10 09:35:21 +0100

    route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CryptoAccountController;
+use App\Http\Controllers\CryptoPriceController;
+use App\Http\Controllers\TransactionController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +16,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Private Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('crypto-accounts', CryptoAccountController::class);
+    
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::post('/transactions', [TransactionController::class, 'store']);
+    Route::get('/transactions/{transaction}',[TransactionController::class, 'show']);
+    
+    Route::post('/convert', [CryptoPriceController::class, 'convertBalance']);
 });
