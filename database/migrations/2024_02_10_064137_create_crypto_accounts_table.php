35684a4d31a6ee510a37d64f6080ca01ce1e586a
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-02-10 09:34:51 +0100

    database tables

--- database/migrations/2024_02_10_064137_create_crypto_accounts_table.php
+++ database/migrations/2024_02_10_064137_create_crypto_accounts_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('crypto_accounts', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->string('phone')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('coin_type'); // For example: Bitcoin, Litecoin, Ethereum, etc.
+            $table->decimal('balance', 18, 8)->default(0); // Balance stored as decimal to handle fractional values
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('crypto_accounts');
     }
 };
